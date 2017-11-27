ded1e4e2de8391b2f44f5dede6206380021fa83a
Thomas Massmann <thomas.massmann@it-spir.it>
2017-11-27 12:02:08 -0500

    Run GS profile after mosaic has been installed.

--- src/collective/tiles/sliders/setuphandlers.py
+++ src/collective/tiles/sliders/setuphandlers.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """Post install import steps for collective.tiles.sliders."""
 
+from collective.tiles.sliders import config
 from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.GenericSetup.interfaces import IProfileImportedEvent
+from zope.component import adapter
 from zope.interface import implementer
 
 
@@ -13,8 +16,8 @@ class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
-            'collective.tiles.sliders:mosaic_support',
-            'collective.tiles.sliders:uninstall',
+            config.MOSAIC_SUPPORT_PROFILE,
+            config.UNINSTALL_PROFILE,
         ]
 
 
@@ -32,3 +35,16 @@ def post_install(context):
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+@adapter(IProfileImportedEvent)
+def handle_profile_imported_event(event):
+    """Update 'last version for profile' after a full import."""
+    qi = api.portal.get_tool(name='portal_quickinstaller')
+    setup = api.portal.get_tool(name='portal_setup')
+
+    if not qi.isProductInstalled(config.PROJECT_NAME):
+        return
+
+    if event.profile_id == 'profile-plone.app.mosaic:default':
+        setup.runAllImportStepsFromProfile(config.MOSAIC_SUPPORT_PROFILE)
